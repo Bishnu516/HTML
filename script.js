d9a2fb74fb8ddbf32dfcb350a100f4f34dba3a24
Bishnu Prasad Behera <bishnuprasadb516@gmail.com>
2024-11-11 14:37:24 +0530

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/*let age=19;
+if(age>18){
+    console.log("you can vote");
+
+}
+else{
+    console.log("you can not vote");
+
+}*/
+/*let num=23;
+if( num%2===0){
+    console.log( num ,"even")
+}
+else{
+    console.log(num ,"odd")
+}*/
+/*alert("I Love U Bishnu");
+let  name =prompt("hello")
+console.log(name);
+let str ="javascript"
+//console.log("string size=",size)*/
+let gameNum=25;
+let userNum=prompt("Guess the game number");
+while(userNum!=gameNum){
+    userNum=prompt("u enter wrong num again guess")
+
+}
+/*console.log(" congratulation u enter right number ")
+let marks=[45,58,78,51,96,87]
+let sum=0;
+for(let val of marks){
+    console.log(marks)
+}*/
